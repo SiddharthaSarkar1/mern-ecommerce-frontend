527799b81ef86ff000879bb595d6d70119f19e4e
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-03-02 03:03:57 +0530

    product feature update

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -6,3 +6,19 @@ export function fetchAllProduct() {
     resolve({ data });
   });
 }
+
+export function fetchProductsByFilters(filter) {
+  let queryString = "";
+
+  for (let key in filter) {
+    queryString += `${key}=${filter[key]}&`;
+  }
+  // console.log(queryString)
+  return new Promise(async (resolve) => {
+    const response = await fetch(
+      "http://localhost:5050/products?" + queryString
+    );
+    const data = response.json();
+    resolve({ data });
+  });
+}
--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -1,16 +1,23 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { fetchAllProduct } from "./productAPI";
+import { fetchAllProduct, fetchProductsByFilters } from "./productAPI";
 
 const initialState = {
   products: [],
   status: "idle",
 };
 
-
 export const fetchAllProductAsync = createAsyncThunk(
   "product/fetchAllProduct",
   async () => {
     const response = await fetchAllProduct();
+    return response.data;
+  }
+);
+
+export const fetchProductsByFiltersAsync = createAsyncThunk(
+  "product/fetchProductsByFilters",
+  async (filter) => {
+    const response = await fetchProductsByFilters(filter);
     return response.data;
   }
 );
@@ -29,6 +36,14 @@ export const productSlice = createSlice({
         state.status = "loading";
       })
       .addCase(fetchAllProductAsync.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.products = action.payload;
+      })
+
+      .addCase(fetchProductsByFiltersAsync.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchProductsByFiltersAsync.fulfilled, (state, action) => {
         state.status = "idle";
         state.products = action.payload;
       });
