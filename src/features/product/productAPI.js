28b69b5686ef91fad66c9a74205120ece73af576
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-03-03 02:41:31 +0530

    pagination functionality

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -7,13 +7,32 @@ export function fetchAllProduct() {
   });
 }
 
-export function fetchProductsByFilters(filter) {
-  let queryString = "";
+export function fetchProductsByFilters(filter, sort, pagination) {
 
+  //filter = {"category": ["smartphone", "laptops"]};
+  //sort = {_sort: "price", _order: "desc"}
+  //http://localhost:5050/products?_sort=price&_order=desc
+  //Pagination = {_page: 1, _limit=10}
+  //http://localhost:5050/products?_page=1&_limit=10
+  //TODO: on server we will support multi values
+  let queryString = "";
   for (let key in filter) {
-    queryString += `${key}=${filter[key]}&`;
+    const categoryValues = filter[key];
+    if(categoryValues.length > 0){
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
   }
-  // console.log(queryString)
+
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`
+  }
+
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`
+  }
+
+  console.log(queryString)
   return new Promise(async (resolve) => {
     const response = await fetch(
       "http://localhost:5050/products?" + queryString
