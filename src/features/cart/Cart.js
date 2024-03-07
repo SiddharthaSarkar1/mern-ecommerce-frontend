2279a5bb670fa9beb295d66b65295d3133cdf8d2
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-03-08 02:16:08 +0530

    checkout page cart section functionality

--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import {
   deleteItemsFromCartAsync,
@@ -31,6 +31,7 @@ export default function Cart() {
 
   return (
     <>
+      {!items.length && <Navigate to="/" replace={true}></Navigate>}
       <div className="mx-auto mt-12 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
         <h1 className="text-4xl font-bold tracking-tight text-gray-900">
           Cart
